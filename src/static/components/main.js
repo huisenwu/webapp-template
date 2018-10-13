640ed1a07d759f6aaade0974da350fd3555e5121
Rodney Gordon <rodney.c.gordon@charter.com>
2018-10-13 17:49:31 -0500

    fixed bug

--- src/static/components/main.js
+++ src/static/components/main.js
@@ -23,16 +23,16 @@ class Main extends PureComponent {
         //ex. http://localhost:8080/api/ambassadors?tags=Apartment&tags=Education
         fetch(`/api/ambassadors?tags=${query.join("tags=")}`).then(response => response.json()).then(ambassadors => this.setState({ambassadors, firstName: "", lastName: ""}));
     };
-    handleChange(name) {
-        this.setState({[name]: event.target.value}, () => this.onNameSearch());
+    handleChange(e, name) {
+        this.setState({[name]: e.target.value}, () => this.onNameSearch());
     }
     render() {
         return (
             <div>
                 <Header />
                 <div style={{height: "85px", display: "flex", justifyContent: "space-around", alignContent: "center", backgroundColor: "#DCDCDC"}}>
-                    <TextField label="First Name" onChange={() => this.handleChange("firstName")} margin="normal" value={this.state.firstName}/>
-                    <TextField label="Last Name" onChange={() => this.handleChange("lastName")} margin="normal" value={this.state.lastName}/>
+                    <TextField label="First Name" onChange={(e) => this.handleChange(e, "firstName")} margin="normal" value={this.state.firstName}/>
+                    <TextField label="Last Name" onChange={(e) => this.handleChange(e, "lastName")} margin="normal" value={this.state.lastName}/>
                     <Search onSearch={this.onTagSearch} searchType="skill" searchName="Skills" placeHolder="ex. legal services" />
                     <Search onSearch={this.onTagSearch} searchType="language" searchName="Language" placeHolder="ex. Spanish, Italian" />
                     <FormControlLabel control={<Switch value="Available"/>} label="Available" />
